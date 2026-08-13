02b67d9a4eef68b4c350ef21c11c15ed21f667e2
SIVASUBRAMANIYAN M <152172345+SivasubramaniyanM@users.noreply.github.com>
2026-08-13 19:53:48 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int num = sc.nextInt()();
 
         if (num % 2 != 0) {
-            System.out.println(num + " is an Odd number");
+            System.out.println(num + " is an Odd numberrrr.");
         } else {
             System.out.println(num + " is an Even number.");
         }

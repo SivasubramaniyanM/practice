293da1a2b092bf4287792e994baa1d2266cfedcd
SIVASUBRAMANIYAN M <152172345+SivasubramaniyanM@users.noreply.github.com>
2026-08-13 20:25:20 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 
 
-        int num = sc.nextInt();
+        int num = sc.nextInt()();
 
         if (num % 2 != 0) {
             System.out.println(num + " is an Odd numberrrr.");
